5c3bcc7992e62574555c216aaa42204356662a1b
cat <marvinarivera@gmail.com>
2021-04-10 00:33:34 -0400

    Portfolio Setup #1

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brown: { 5: '#A1855D' }
+      },
+    },
   },
   variants: {
     extend: {},
